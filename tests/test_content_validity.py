4ed706ece0ad8fd49f837abfb34ac9644dd34e71
kudanai <kudanai@gmail.com>
2023-12-23 21:46:57 +0500

    update tests

--- tests/test_content_validity.py
+++ tests/test_content_validity.py
@@ -1,5 +1,5 @@
 import re
-from . import TRANSLATION_FILE
+from . import QuranCorpus
 
 
 def test_no_bom():
@@ -7,9 +7,8 @@ def test_no_bom():
     Translation file should not contain BOM's
     as per Tanzil directive
     """
-    with open(TRANSLATION_FILE, "r") as f:
-        trans = f.read()
-        assert "\uFEFF" not in trans
+    trans = QuranCorpus.read_all()
+    assert "\uFEFF" not in trans
 
 
 def test_no_repeated_fili():
@@ -20,10 +19,9 @@ def test_no_repeated_fili():
     error_list = []
     pattern = re.compile("[\u07A6-\u07AB]{2}")
 
-    with open(TRANSLATION_FILE, "r") as f:
-        for line in f:
-            if re.findall(pattern, line):
-                sura, ayat, trans = line.split("|")
-                error_list.append(f"{sura}|{ayat}")
+    for line in QuranCorpus.read_lines():
+        if re.findall(pattern, line):
+            sura, ayat, trans = line.split("|")
+            error_list.append(f"{sura}|{ayat}")
 
     assert len(error_list) == 0, f"Repeated fili found on {error_list}"
